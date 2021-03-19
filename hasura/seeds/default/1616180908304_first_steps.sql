6c7c399074c9b522e3537ba2f879a70cbce5a8e4
Adron Hall <adron@hasura.io>
2021-03-19 12:20:38 -0700

    Adding the seeds of our application tree!

---
+++ hasura/seeds/default/1616180908304_first_steps.sql
@@ -0,0 +1,9 @@
+INSERT INTO coreusersystem."user" (name) VALUES ('John Smith');
+INSERT INTO coreusersystem."user" (name) VALUES ('Sally Smith');
+INSERT INTO coreusersystem."user" (name) VALUES ('Moha Jackson');
+INSERT INTO coreusersystem."user" (name) VALUES ('Elizabeth Warren');
+INSERT INTO coreusersystem."user" (name) VALUES ('Donnie Ferguson');
+INSERT INTO coreusersystem."user" (name) VALUES ('Chuck Schuldiner');
+INSERT INTO coreusersystem."user" (name) VALUES ('Yngwie Malmsteen');
+INSERT INTO coreusersystem."user" (name) VALUES ('Gracie Abrahams');
+INSERT INTO coreusersystem."user" (name) VALUES ('Onur Haxcited Yukruk');
